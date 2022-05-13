5179de3cb3056e69d9e14d7c30f733305a015403
Paweł Chmiel <pawelchmiel.20.03@gmail.com>
2022-05-13 16:44:52 +0200

    pubspecs updated

--- video_player/example/ios/Runner/GeneratedPluginRegistrant.m
+++ video_player/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -18,18 +18,11 @@
 @import path_provider_ios;
 #endif
 
-#if __has_include(<video_player_avfoundation/FLTVideoPlayerPlugin.h>)
-#import <video_player_avfoundation/FLTVideoPlayerPlugin.h>
-#else
-@import video_player_avfoundation;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
-  [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
 }
 
 @end
